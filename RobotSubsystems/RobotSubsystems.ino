971e69660071f5997c1f4320c9ccaab43b9dab26
jamartiney <56280907+jamartiney@users.noreply.github.com>
2025-06-06 06:40:26 -0400

    Commented out all subsystems in prep for camp.

--- RobotSubsystems/RobotSubsystems.ino
+++ RobotSubsystems/RobotSubsystems.ino
@@ -4,43 +4,43 @@
 // Date:              2025/05/16
 /////////////////////////////////////////////////
 
-#include <BlinkSubsystem.h>
-#include <KeypadSubsystem.h>
-#include <SerialSubsystem.h>
-// #include <BuzzerSubsystem.h>
-#include <MelodySubsystem.h>
+//#include <BlinkSubsystem.h>
+//#include <SerialSubsystem.h>
+//#include <KeypadSubsystem.h>
+//#include <BuzzerSubsystem.h>
+//#include <MelodySubsystem.h>
 
-KeypadSubsystem m_keypad = KeypadSubsystem();     // Create keypad
-BlinkSubsystem m_blink = BlinkSubsystem(250);     // Create blink w/ 250 ms period
-SerialSubsystem m_serial = SerialSubsystem(9600); // Create serial at 9600 baud
-// BuzzerSubsystem m_buzzer = BuzzerSubsystem(12); //Create buzzer on port 12
-MelodySubsystem m_melody = MelodySubsystem(12, 500); //Create melody on port 12 w/ 1000 duration
+//BlinkSubsystem m_blink = BlinkSubsystem(250);     // Create blink w/ 250 ms period
+//SerialSubsystem m_serial = SerialSubsystem(9600); // Create serial at 9600 baud
+//KeypadSubsystem m_keypad = KeypadSubsystem();     // Create keypad
+//BuzzerSubsystem m_buzzer = BuzzerSubsystem(12); //Create buzzer on port 12
+//MelodySubsystem m_melody = MelodySubsystem(12, 500); //Create melody on port 12 w/ 1000 duration
 
 void setup()
 {
-  m_keypad.setup(); // set the keypad subsystem
-  m_blink.setup();  // setup the LED blink subsystem
-  m_serial.setup(); // setup the serial subsystem
-  // m_buzzer.setup();  //setup the buzzer subsystem
-  m_melody.setup();  //setup the melody subsystem
+  //m_blink.setup();  // setup the LED blink subsystem
+  //m_serial.setup(); // setup the serial subsystem
+  //m_keypad.setup(); // set the keypad subsystem
+  //m_buzzer.setup();  //setup the buzzer subsystem
+  //m_melody.setup();  //setup the melody subsystem
 }
 
-char keypadKey;
-int keyIndex;
+//char keypadKey;
+//int keyIndex;
 
 void loop()
 {
-  m_keypad.loop(); // execute the keypad loop
-  m_blink.loop();  // execute the blink loop
-  m_serial.loop(); // execute the serial loop
-  // m_buzzer.loop();  //execute the buzzer loop
+  //m_blink.loop();  // execute the blink loop
+  //m_serial.loop(); // execute the serial loop
+  //m_keypad.loop(); // execute the keypad loop
+  //m_buzzer.loop();  //execute the buzzer loop
   //m_melody.loop(); //execute the melody loop
 
-  if (m_keypad.hasKey())
-  {
-    keypadKey = m_keypad.getKey();
-    m_serial.printKey(keypadKey);
-    keyIndex = keypadKey - '0';
-    //m_melody.selectNote(keyIndex);
-  }
+  //if (m_keypad.hasKey())
+  //{
+  //  keypadKey = m_keypad.getKey();
+  //  m_serial.printKey(keypadKey);
+  //  keyIndex = keypadKey - '0';
+  //  m_melody.selectNote(keyIndex);
+  //}
 }
