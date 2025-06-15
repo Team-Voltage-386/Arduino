ce425a6980b4d423951b48c40e687aec82348970
jamartiney <56280907+jamartiney@users.noreply.github.com>
2025-06-15 16:50:18 -0400

    Added 05 Joystick Subsystem

--- libraries/JoystickSubsystem/JoystickSubsystem.cpp
+++ libraries/JoystickSubsystem/JoystickSubsystem.cpp
@@ -13,52 +13,14 @@ const int JoystickSubsystem::Y_pin = 1; // analog pin connected to Y output
 // <<constructor>>
 JoystickSubsystem::JoystickSubsystem()
 {
-  prevMillis = 0;
-  currMillis = 0;
-  delaytime1 = 500;
-  dataIsReady = false;
-
   joystickData.button = false;
   joystickData.x = 0;
   joystickData.y = 0; 
-
-  joystickSavedData.button = false;
-  joystickSavedData.x = 0;
-  joystickSavedData.y = 0;
-
-  savedDataError = false; // Initialize saved data error flag
-}
-
-bool JoystickSubsystem::isDataReady()
-{
-  return dataIsReady;
 }
 
 JoystickSubsystem::JoystickData JoystickSubsystem::getData()
 {
   return joystickData;
-}
-
-JoystickSubsystem::JoystickData JoystickSubsystem::getSavedData()
-{
-  if (!dataIsReady) {
-    // If data is not ready, flag an error
-    savedDataError = true;
-  }
-
-  dataIsReady = false; // Reset data ready flag
-
-  return joystickSavedData;
-}
-
-bool JoystickSubsystem::hasSavedDataError()
-{
-  return savedDataError; // Return the saved data error flag status
-}
-
-void JoystickSubsystem::clearSavedDataError()
-{
-  savedDataError = false; // Clear the saved data error flag
 }
 
 // Call this in setup() function
@@ -71,19 +33,6 @@ void JoystickSubsystem::setup()
 // Call this in loop() function
 void JoystickSubsystem::loop()
 {
-  // save the current ms step count
-  currMillis = millis();
-
-  // if curr - prev is greater or equal to blink interval, blink
-  if (currMillis - prevMillis >= delaytime1)
-  {
-    prevMillis = currMillis;
-    dataIsReady = true; // Set data as ready to be read
-    joystickSavedData.button = digitalRead(SW_pin);
-    joystickSavedData.x = analogRead(X_pin);
-    joystickSavedData.y = analogRead(Y_pin);
-  }
-
   joystickData.button = digitalRead(SW_pin);
   joystickData.x = analogRead(X_pin);
   joystickData.y = analogRead(Y_pin);
--- libraries/JoystickSubsystem/JoystickSubsystem.h
+++ libraries/JoystickSubsystem/JoystickSubsystem.h
@@ -17,32 +17,18 @@ public:
     unsigned int y;
   };
 
-  bool isDataReady();
-  JoystickData getData();
-  JoystickData getSavedData();
-  bool hasSavedDataError();
-  void clearSavedDataError();
-  
   JoystickSubsystem();
+  JoystickData getData();
   void setup();
   void loop();
 
 private:
   JoystickData joystickData;
-  JoystickData joystickSavedData;
-
-  unsigned long prevMillis;
-  unsigned long currMillis;
-  unsigned long delaytime1;
 
   // Arduino pin numbers
   static const int SW_pin; // digital pin connected to switch output
   static const int X_pin; // analog pin connected to X output
   static const int Y_pin; // analog pin connected to Y output  
-
-  bool dataIsReady;
-  bool savedDataError;
-
 };
 
 #endif
